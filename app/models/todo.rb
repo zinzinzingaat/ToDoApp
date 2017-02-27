5d616c8579cf750f0e16189af4477a0c5470082c
aniketrao92 <aniket.rao92@gmail.com>
2017-02-27 08:25:11 +0000

    Added validations

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,3 +1,4 @@
 class Todo < ApplicationRecord
-  
+  validates :name, presence: true, length: {minimum: 3, maximum: 15}
+  validates :description, presence: true, length:{minimum: 10, maximum:300}
 end
