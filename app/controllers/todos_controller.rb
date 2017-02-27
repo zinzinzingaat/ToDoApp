2c4943bf48d445232884628bea82232720065f76
aniketrao92 <aniket.rao92@gmail.com>
2017-02-27 07:48:53 +0000

    Added 'show' and 'create' actions

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -4,5 +4,24 @@ def new
   @todos = Todo.new
 end
 
+def create
+  @todos = Todo.create(todo_params)
+  if @todos.save
+    flash[:notice] = "The todo was created successfully."
+    redirect_to todo_path(@todos)
+  else
+    render 'new'
+  end
+end
 
+def show
+  @todos = Todo.find(params[:id])
+end
+
+
+
+private
+def todo_params
+  params.require(:todo).permit(:name,:description)
+end
 end
